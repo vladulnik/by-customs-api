5bb91a79d7b36ef88eea7048386a11f02553c84c
vladulnik <vladislavmoiseev9@gmail.com>
2025-06-06 23:45:02 +0300

    Initial commit

---
+++ src/main/java/by/customs/by_customs_api/TechnologicalPracticeApplication.java
@@ -0,0 +1,13 @@
+package by.customs.by_customs_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologicalPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologicalPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/customs/by_customs_api/TechnologicalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package by.customs.by_customs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnologicalPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
