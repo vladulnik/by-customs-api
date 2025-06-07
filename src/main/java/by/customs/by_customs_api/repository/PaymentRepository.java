1ad882f3ed069a9c5d380b0ae7ff064355679be2
vladulnik <vladislavmoiseev9@gmail.com>
2025-06-07 17:05:00 +0300

    Interfaces added

---
+++ src/main/java/by/customs/by_customs_api/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package by.customs.by_customs_api.repository;
+
+import by.customs.by_customs_api.entity.PaymentEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PaymentRepository extends JpaRepository<PaymentEntity, Long> {
+}
